f4b1f470ee7d7914ad5a90f46d531044846c1486
BENwada <domo0103wada@gmail.com>
2023-09-08 08:37:50 +0900

    cart add

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,23 +3,43 @@ import React from "react";
 
 const CartPage = () => {
   return (
-    <div className="">
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500">
       {/* PRODUCTS CONTAINER */}
-      <div className="">
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll">
         {/* SINGLE ITEM */}
-        <div className="">
+        <div className="flex items-center justify-between mb-4 ">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
-            <h1>sicilian</h1>
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
             <span>Large</span>
           </div>
-          <h2>$79.90</h2>
-          <span>X</span>
+          <h2 className="font-bold">$79.90</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
+        {/* SINGLE ITEM */}
+        <div className="flex items-center justify-between mb-4 ">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold">$79.90</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
+        {/* SINGLE ITEM */}
+        <div className="flex items-center justify-between mb-4 ">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold">$79.90</h2>
+          <span className="cursor-pointer">X</span>
         </div>
       </div>
       {/* PAYMENT CONTAINER */}
-      <div className="">
-        <div className="">
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center">
+        <div className="flex justify-between">
           <span>Subtotal(3 items)</span>
           <span>$81.70</span>
         </div>
